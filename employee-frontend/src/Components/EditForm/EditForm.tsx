e03fc3e863e907c6d982e07a08f8acd5d176cc61
BurnedBySnow <sebastian.karlsson97@gmail.com>
2022-08-16 12:12:30 +0200

    update to EditForm

--- employee-frontend/src/Components/EditForm/EditForm.tsx
+++ employee-frontend/src/Components/EditForm/EditForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Employee, Property } from "../Types";
+import { Employee } from "../Types";
 import {
   ButtonContainer,
   CancelButton,
